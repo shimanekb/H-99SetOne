81e4e5deb153bd97570895302d3993ac987a4ab3
shimanekb <brandon.shimanek@hotmail.com>
2015-06-14 17:27:38 -0500

    Created problem7 made some headway with recursive data type. not finished

--- problem7/src/H99/Problem7.hs
+++ problem7/src/H99/Problem7.hs
@@ -2,6 +2,8 @@ module H99.Problem7
 (myFlatten, NestedList) where
 
 data NestedList a = Elem a | List [NestedList a]
+                    deriving (Show)
 
-myFlatten :: NestedList -> [a] -> [a]
-myFlatten (Elem x) concatXs = myFlatten y (x:concatXs)  
+myFlatten :: NestedList  a -> [a] -> NestedList a
+myFlatten (List xs)  ys = let nestHead = head xs
+                          in  nestHead
